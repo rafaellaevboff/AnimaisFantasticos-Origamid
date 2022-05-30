22cec5b498d26d43c1ed6b57e40cd5b36cfcc762
rafaellaevboff <rafaella-eboff@rede.ulbra.br>
2022-05-30 16:06:56 -0300

    Bubble e ClickOutside

--- assets/js/modules/dropdown-menu.js
+++ assets/js/modules/dropdown-menu.js
@@ -1,17 +1,19 @@
+import outsideClick from "./outsideClick.js";
+
 export default function initDropDownMenu(){
-    
+    const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+
+    dropdownMenus.forEach(menu => {
+        ['touchstart', 'click'].forEach(userEvent =>{
+            menu.addEventListener(userEvent, handleClick);
+        })
+    });
+
+    function handleClick(event){
+        event.preventDefault();
+        this.classList.add('active');
+        outsideClick(this, ['touchstart', 'click'], ()=>{
+            this.classList.remove('active')
+        });
+    };
 }
-
-const dropdownMenus = document.querySelectorAll('[data-dropdown]');
-
-dropdownMenus.forEach(menu => {
-    ['touchstart', 'click'].forEach(userEvent =>{
-        menu.addEventListener(userEvent, handleClick);
-    })
-});
-
-function handleClick(event){
-    event.preventDefault();
-    this.classList.toggle('active');
-};
-
