54d64e0cd15de8ef34177052e36cccef20b09d08
rafaellaevboff <rafaella-eboff@rede.ulbra.br>
2022-06-21 15:14:37 -0300

    refatoração scroll-suave

--- assets/js/modules/scroll-suave.js
+++ assets/js/modules/scroll-suave.js
@@ -1,26 +1,33 @@
-export default function initScroolSuave() {
-    const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+export default class ScrollSuave {
+    constructor(links, options) {
+        this.linksInternos = document.querySelectorAll(links);
+        if (options === undefined) {
+            this.options = { behavior: 'smooth', block: 'start' };
+        } else {
+            this.options = options;
+        }
+        this.scrollToSection = this.scrollToSection.bind(this);
+        // o bind vai declarar qual vai ser o this
+        // nesse caso se scrollToSection for usado dps vai se referenciar ao que foi passado no bind
+    }
 
-    function scrollToSection(event) {
+    scrollToSection(event) {
         event.preventDefault();
         const href = event.currentTarget.getAttribute('href');
         const section = document.querySelector(href);
-        section.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-        }); // não é todo browser que suporta essa função de scroll suave
-
-        // forma alternativa de fazer o scroll suave
-        // const topo = section.offsetTop;
-        // window.scrollTo({
-        //     top: topo,
-        //     behavior: 'smooth',
-        // });
+        section.scrollIntoView(this.options);
     }
 
-    if (linksInternos.length) {
-        linksInternos.forEach(((link) => {
-            link.addEventListener('click', scrollToSection);
-        }));
+    addLinkEvent() {
+        this.linksInternos.forEach((link) => {
+            link.addEventListener('click', this.scrollToSection);
+        });
+    }
+
+    init() {
+        if (this.linksInternos.length) {
+            this.addLinkEvent();
+        }
+        return this;
     }
 }
