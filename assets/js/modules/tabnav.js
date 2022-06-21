742162e34c711f3b008bbb27823c56a7c88513df
rafaellaevboff <rafaella-eboff@rede.ulbra.br>
2022-06-21 16:03:00 -0300

    Refatoração tabnav - tranformando em classe

--- assets/js/modules/tabnav.js
+++ assets/js/modules/tabnav.js
@@ -1,31 +1,33 @@
 // é bom isolar o escopo para que se possa reutilizar as variáveis durante o código
-export default function initTabNav() {
-    // navegação por tab:
-    const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-    const tabContent = document.querySelectorAll('[data-tab="content"] section');
-    const ativo = 'ativo';
-
-    function activeTab(index) {
-        tabContent.forEach((section) => {
-            section.classList.remove(ativo);
-        });
-        const direcao = tabContent[index].dataset.anime;
-        tabContent[index].classList.add(ativo, direcao);
+export default class TabNav {
+    constructor(menu, content) {
+        this.tabMenu = document.querySelectorAll(menu);
+        this.tabContent = document.querySelectorAll(content);
+        this.activeClass = 'ativo';
     }
-    // a função acima faz tds as sections retirarem a classe ativo 1º
-    // dps add a classe ativo na section selecionada
 
-    // fazer verificação se os itens acima existem na página: (se o length for 0 significa false)
-    if (tabMenu.length && tabContent.length) {
-        tabContent[0].classList.add(ativo); // ao abrir a página aparecer conteúdo
-
-        tabMenu.forEach((itemMenu, index) => {
-            itemMenu.addEventListener('click', () => {
-                activeTab(index);
-                // ao clicar na imagem vai chamar a função criada acima dessa atual, a activeTab
-            });
+    // ativa a tab de acordo com o index da mesma
+    activeTab(index) { // método
+        this.tabContent.forEach((section) => {
+            section.classList.remove(this.activeClass);
         });
-        // foi feito por index, mas pode ser feito através de classe
-        // caso o conteúdo não esteja na ordem ou se preferir fazer com classe
+        const direcao = this.tabContent[index].dataset.anime;
+        this.tabContent[index].classList.add(this.activeClass, direcao);
+    }
+
+    // add os eventos nas tabs
+    addTabNavEvent() {
+        this.tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => this.activeTab(index));
+            // ao clicar na imagem vai chamar a função criada acima dessa atual, a activeTab
+        });
+    }
+
+    init() {
+        if (this.tabMenu.length && this.tabContent.length) {
+            // ativar primeiro item
+            this.activeTab(0);
+            this.addTabNavEvent();
+        }
     }
 }
