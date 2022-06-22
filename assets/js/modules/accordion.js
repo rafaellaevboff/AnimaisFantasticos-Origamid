9f4f5093d79ae0e05c7fc2de3755c8f08491afaa
rafaellaevboff <rafaella-eboff@rede.ulbra.br>
2022-06-22 10:23:14 -0300

    add this no retorno das classes

--- assets/js/modules/accordion.js
+++ assets/js/modules/accordion.js
@@ -22,5 +22,6 @@ export default class Accordion {
             this.toggleAccordion(this.accordionList[0]); // ativa o primeiro item
             this.addAccordionEvent();
         }
+        return this;
     }
 }
